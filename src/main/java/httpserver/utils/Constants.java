1db73b974a947a574c01210e7dd92df3d92233a4
Nadia Collado <collado.nadia@gmail.com>
2022-02-18 10:11:51 -0500

    Handle GET request with 200 level text response

--- src/main/java/httpserver/utils/Constants.java
+++ src/main/java/httpserver/utils/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static String DOUBLE_LINE_BREAK = "\r\n\r\n";
     public static String PROTOCOL = "HTTP/1.1";
     public static String ALLOW = "Allow";
+    public static String LOCATION = "Location";
+    public static String TYPE = "Content-Type";
 }
