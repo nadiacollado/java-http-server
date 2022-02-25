dd5fcc5dba3039cb4be23302a585bbb1facdd215
Nadia Collado <collado.nadia@gmail.com>
2022-02-25 18:03:50 -0500

    Add MockRequestReader to RequestParser tests

--- src/test/java/httpserver/mocks/MockReader.java
+++ src/test/java/httpserver/mocks/MockReader.java
@@ -5,9 +5,7 @@ import java.io.Reader;
 public class MockReader extends Reader {
 
     @Override
-    public int read(char[] cbuf, int off, int len) {
-        return 0;
-    }
+    public int read(char[] container, int off, int len) { return 0; }
 
     @Override
     public void close() {}
